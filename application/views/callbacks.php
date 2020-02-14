8724c05a10ce3390f5d90204992af97ea147863b
preetika <shiva@seconddigital.com>
2020-02-14 11:16:03 +0530

    black table color

--- application/views/callbacks.php
+++ application/views/callbacks.php
@@ -65,9 +65,15 @@
                 </div>
                     <!-- //header-ends -->
                        <div>
-					   <style>
+					 
+<div class="container">
+  
+    <div class="page-header">
+        <h1><?php echo $heading; ?></h1>
+    </div>
+    <style>
     @media screen and (min-width: 768px) {
-        modal_
+      
         .modal-dialog  {
             width:900px;
         }
@@ -103,12 +109,59 @@
         border: 1px solid #aaa;
         padding: 5px
     }
+    @media (max-width: 991px){
+        .priority-7,.priority-8,.priority-10,.priority-11,.priority-12{
+			display:none;
+		}
+        #search_form{
+            display:none;
+        }
+        }
+        /* @media (max-width: 1150px){
+            .priority-9, .priority-7,.priority-8,.priority-11,.priority-12,.priority-13{
+			display:none;
+		}
+
+        } */
+
+       
+
+     
+	@media screen and (max-width: 900px) and (min-width: 550px) {
+        #search_form{
+            display:none;
+        }
+		.priority-4,.priority-5,.priority-6, .priority-7,.priority-8,.priority-9,.priority-10,.priority-11{
+			display:none;
+		}
+        .priority-12{
+			display:none;
+		}
+	}
+	
+	@media screen and (max-width: 550px) {
+        .priority-4,.priority-5,.priority-6, .priority-7,.priority-8,.priority-9,.priority-10,.priority-11{
+			display:none;
+		}
+        #search_form{
+            display:none;
+        }
+        .priority-12{
+			display:none;
+		}
+	}
+	
+	@media screen and (max-width: 300px) {
+        #search_form{
+            display:none;
+        }
+        .priority-4,.priority-5,.priority-6, .priority-7,.priority-8,.priority-9,.priority-10,.priority-11{
+			display:none;
+		}
+   
+	
+	}
 </style>
-<div class="container">
-  
-    <div class="page-header">
-        <h1><?php echo $heading; ?></h1>
-    </div>
     <form method="POST" id="search_form">
         <?php if($this->session->userdata("user_type")=="manager") { ?>
             <input type="hidden" name="self" id="self_input" value="<?php echo $this->session->userdata('self'); ?>">
@@ -278,83 +331,88 @@
     <?php } ?>
     <br>
 
-</div>
-<div class="container">
-    <table id="example" class="table table-striped table-bordered dt-responsive" cellspacing="0" width="100%">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Contact Name</th> 
-                <th>Contact No</th>
-                <th>Email</th>
-                <th>Project</th>
-                <?php if($this->session->userdata("user_type")!="user") { ?>
-                    <th>Lead Source</th>
-                    <th>Lead Id</th> 
-                <?php } ?>
-                <th>Advisor</th>
-                <?php if($this->session->userdata("user_type")!="user") { ?> 
-                    <th>Sub-Source</th>
-                <?php } ?>
-                <th>Due date</th>
-                <th>Status</th>
-                <th>Date Added</th>
-                <!-- <th>Last Update</th> -->
-                <th>Action</th>
-            </tr>
-        </thead> 
-        <tbody id="main_body">
-            <?php $i= 1;
-            if(count($result)>0){
-            foreach ($result as $data) {
-                $duedate = explode(" ", $data->due_date);
-                $duedate = $duedate[0]; ?>
-                <tr id="row<?php echo $i ?>" <?php if(strtotime($duedate)<strtotime('today')){?> class="highlight_past" <?php }elseif(strtotime($duedate) == strtotime('today')) {?> class="highlight_now" <?php }elseif(strtotime($duedate)>strtotime('today')){ ?> class="highlight_future" <?php } ?>>
-                    <td><?php echo $i; ?></td>
-                    <td><?php echo $data->name; ?></td>
-                    <td><?php echo $data->contact_no1 ?></td>
-                    <td><?php echo $data->email1; ?></td>
-                    <td><?php echo $data->project_name; ?></td>
+
+    <div class="container">
+        <table id="example" class="table table-striped table-bordered dt-responsive" cellspacing="0" width="100%">
+            <thead>
+                <tr>
+                    <th class="priority-1">No</th>
+                    <th class="priority-2">Contact Name</th> 
+                    <th class="priority-3">Contact No</th>
+                    <th class="priority-4">Email</th>
+                    <th class="priority-5">Project</th>
                     <?php if($this->session->userdata("user_type")!="user") { ?>
-                        <td><?php echo $data->lead_source_name; ?></td>
-                        <td><?php echo $data->leadid; ?></td>
+                        <th class="priority-6">Lead Source</th>
+                        <th class="priority-7">Lead Id</th> 
                     <?php } ?>
-                    <td><?php echo $data->user_name; ?></td>
-                    <?php if($this->session->userdata("user_type")!="user") { ?>
-                        <td><?php echo $data->broker_name; ?></td>
+                    <th class="priority-8">Advisor</th>
+                    <?php if($this->session->userdata("user_type")!="user") { ?> 
+                        <th>Sub-Source</th>
                     <?php } ?>
-                    <td class="due_date"><?php echo $data->due_date; ?></td>
-                    <td><?php echo $data->status_name; ?></td>
-                    <td><?php echo $data->date_added; ?></td>
-                    <!-- <td><?php echo $data->last_update; ?></td> -->
-                    <td>
-                        <table>
-                            <tr>
-                                <td>
-                                    <!-- <a onclick="edit('<?php echo $data->id; ?>')" data-toggle="modal" data-target="#modal_edit"> -->
-                                    <a href="<?= base_url('callback-details?id='.$data->id) ?>" target="_blank">
-                                        <i class="fa fa-home fa-2x"  title="Detail" style="color:#ff1122; font-size:21px;padding-right:7px;" aria-hidden="true"></i>
-                                    </a>
-                                </td>
-                                <td>
-                                    <a onclick="previous_callbacks('<?php echo $data->id; ?>')" data-toggle="modal" data-target="#modal_previous">
-                                        <i class="fa fa-keyboard-o fa-2x" title="Notes" style="color:#ff1122; font-size:21px;padding-right:7px;" aria-hidden="true"></i>
-                                    </a>
-                                </td>
-                            </tr>
-                        </table>
-                    </td>
+                    <th class="priority-9">Due date</th>
+                    <th class="priority-10">Status</th>
+                    <th class="priority-11">Date Added</th>
+                    <!-- <th>Last Update</th> -->
+                    <th class="priority-12">Action</th>
                 </tr>
-            <?php $i++; } }?>
-        </tbody>
-    </table>
-</div>
+            </thead> 
+            <tbody id="main_body">
+                <?php $i= 1;
+                if(count($result)>0){
+                foreach ($result as $data) {
+                    $duedate = explode(" ", $data->due_date);
+                    $duedate = $duedate[0]; ?>
+                    <tr id="row<?php echo $i ?>" <?php if(strtotime($duedate)<strtotime('today')){?> class="highlight_past" <?php }elseif(strtotime($duedate) == strtotime('today')) {?> class="highlight_now" <?php }elseif(strtotime($duedate)>strtotime('today')){ ?> class="highlight_future" <?php } ?>>
+                        <td class="priority-1"><?php echo $i; ?></td>
+                        <td class="priority-2"><?php echo $data->name; ?></td>
+                        <td class="priority-3"><?php echo $data->contact_no1 ?></td>
+                        <td class="priority-4"><?php echo $data->email1; ?></td>
+                        <td class="priority-5"><?php echo $data->project_name; ?></td>
+                        <?php if($this->session->userdata("user_type")!="user") { ?>
+                            <td class="priority-6"><?php echo $data->lead_source_name; ?></td>
+                            <td class="priority-7"><?php echo $data->leadid; ?></td>
+                        <?php } ?>
+                        <td class="priority-8"><?php echo $data->user_name; ?></td>
+                        <?php if($this->session->userdata("user_type")!="user") { ?>
+                            <td class="priority-9"><?php echo $data->broker_name; ?></td>
+                        <?php } ?>
+                        <td class="priority-10 due_date"><?php echo $data->due_date; ?></td>
+                        <td class="priority-11"><?php echo $data->status_name; ?></td>
+                        <td class="priority-12"><?php echo $data->date_added; ?></td>
+                        <!-- <td><?php echo $data->last_update; ?></td> -->
+                        <td>
+                            <table>
+                                <tr>
+                                    <td>
+                                        <!-- <a onclick="edit('<?php echo $data->id; ?>')" data-toggle="modal" data-target="#modal_edit"> -->
+                                        <a href="<?= base_url('callback-details?id='.$data->id) ?>" target="_blank">
+                                            <i class="fa fa-home fa-2x"  title="Detail" style="color:#ff1122; font-size:21px;padding-right:7px;" aria-hidden="true"></i>
+                                        </a>
+                                    </td>
+                                    <td>
+                                        <a onclick="previous_callbacks('<?php echo $data->id; ?>')" data-toggle="modal" data-target="#modal_previous">
+                                            <i class="fa fa-keyboard-o fa-2x" title="Notes" style="color:#ff1122; font-size:21px;padding-right:7px;" aria-hidden="true"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                <?php $i++; } }?>
+            </tbody>
+        </table>
+    
     
 <div style="margin-top: 20px">
     <span class="pull-left"><p>Showing <?php echo ($this->uri->segment(2)) ? $this->uri->segment(2)+1 : 1; ?> to <?= ($this->uri->segment(2)+count($result)); ?> of <?= $totalRecords; ?> entries</p></span>
     <ul class="pagination pull-right"><?php echo $links; ?></ul>
 </div>
-<br/><br/><br/><br/><br/><br/><br/><br/>
+</div>
+<br/><br/>
+</div>
+
+
+<br/><br/><br/><br/><br/><br/>
 
 <?php $this->load->view('callback_operations'); ?>
  
